558e48c67b4e0d542bb355c42e8ed59ba331c2a0
smchang <smchang@mit.edu>
2012-04-13 15:53:12 -0400

    tournament sidebar with selecting admin

--- tournament.js
+++ tournament.js
@@ -1,6 +1,29 @@
+var numAdmins = 1;
+var numBoots = 0;
+
 $(document).ready(function(){
     var sidebar = $('#adminbar');
-    $('#expandbar').click(function(){
+    $('#bar').click(function(){
         console.log('expand/collapse sidebar');
+    });
+
+    $('#makeAdminBtn').click(function(){
+        var selected = $('input:checked');
+        var numSelected = selected.length;
+        $(selected).each(function(ind, elt){
+            if($(elt).parent().hasClass('admin')){
+                numSelected--;
+            }else{
+                $(elt).parent().addClass('admin');
+            }
+            elt.checked=false;
+        });
+        numAdmins += numSelected;
+        console.log(numAdmins);
+/*        $('#playerList').children().each(function(ind, elt){
+            if(ind<numAdmins){
+                $(elt).addClass('admin');
+            }
+        });*/
     });
 });
