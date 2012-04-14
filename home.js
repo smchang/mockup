fc95c7c36ec484736d2a78c37dd40473bfc5b9c9
smchang <smchang@mit.edu>
2012-04-14 03:20:14 -0400

    homepage listing close buttons

--- home.js
+++ home.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
+    var makeCloseable = function(listing){
+        var closeButton = $('<button>');
+        closeButton.addClass('closeButton');
+        closeButton.html('&times;');
+        $(listing).append(closeButton);
+    }
+    $('.listing').each(function(ind, elt){
+        makeCloseable(elt);
+    });
 
+    $('.closeButton').click(function(){
+        $(this).parent().remove();
+    });
 });
