b3e6aad7ee2b74dc7e1d035618a55ea815deb3a2
Stephen Chang <smchang@mit.edu>
2012-04-15 16:56:50 -0400

    sidebar collapsing/expanding functionality

--- tournament.js
+++ tournament.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
     var sidebar = $('#adminbar');
     $('#bar').click(function(){
-        console.log('expand/collapse sidebar');
+        $('#adminbar').hide('slide',{direction:'left',distance:'90%'}, function(){$('#expandBar').css('display','block');});
+    });
+
+    $('#expandBar').click(function(){
+        $('#adminbar').show('slide',{direction:'left'});
+        $('#expandBar').css('display','none');
     });
 
     $('#makeAdminBtn').click(function(){
